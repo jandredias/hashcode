ee5aad8f5ccdb9bf0cbfbb4985350368b4482d1a
André Dias <j.andre.dias@tecnico.ulisboa.pt>
2016-02-11 20:49:38 +0000

    asd

--- manager.py
+++ manager.py
@@ -1,8 +1,10 @@
+import itertools
+from drone import Drone
 class Manager:
   def __init__(self):
     self._warehouses = []
     self._orders = []
-    self._drones = 0
+    self._drones = []
     self._turns = 0
     self._weigh = []
     self._payload = 0
@@ -33,12 +35,18 @@ class Manager:
     return self._weigh[productID]
 
   def changeDrones(self, drones):
-    self._drones = drones
+    i = 0
+    for e in range(drones):
+      self._drones += [Drone(i, [0,0])]
+      i += 1
 
   def drones(self):
     return self._drones
 
   def addWarehouse(self, warehouse):
+    if 0 == len(self._warehouses):
+      for e in self._drones:
+        e.changeLocation(warehouse.location())
     self._warehouses += [warehouse]
 
   def organizeWarehouses(self):
@@ -53,4 +61,15 @@ class Manager:
   def orders(self):
     return self._orders
 
- 
+  def computeConflicts(self):
+    for w in self._warehouses:
+      for p in range(self._types):
+        orders = w.ordersByProduct(p)
+        for perm in itertools.permutations(orders): 
+          print()
+        #INSERT LEAL STUFF HERE
+
+  def getResults():
+    for orders in self._orders:
+      drone = self._drones.pop()
+      
